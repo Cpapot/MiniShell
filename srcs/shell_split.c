9f344cf1fb40894c77dc87cc691fb5603507edb9
Corentin Papot <cpapot@z4r6p7.42lyon.fr>
2023-03-07 17:13:06 +0100

    quote

--- srcs/shell_split.c
+++ srcs/shell_split.c
@@ -6,70 +6,11 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 13:47:51 by cpapot            #+#    #+#             */
-/*   Updated: 2023/03/04 17:13:44 by cpapot           ###   ########.fr       */
+/*   Updated: 2023/03/07 17:09:52 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-/*
-static int	count_word(char const *str)
-{
-	int		i;
-	int		count;
-
-	i = 0;
-	count = 0;
-	if (str[0] == ' ')
-		count--;
-	while (str[i] != '\0')
-	{
-		if (str[i] == ' ' && (!((str[i + 1] == ' '))
-				&& str[i + 1] != '\0'))
-			count++;
-		if ((str[i] == '<' || str[i] == '>')
-			&& (str[i + 1] != '<' && str[i + 1] != '>'))
-			count++;
-		if (str[i] == '|')
-			count++;
-		if ((str[i] != '<' && str[i] != '>' && str[i] != ' ')
-			&& (str[i + 1] == '<' || str[i + 1] == '>'))
-			count++;
-		i++;
-	}
-	if (str[0] != '\0')
-		count++;
-	return (count);
-}
-*/
-
-static int	count_word(char const *str)
-{
-	int		i;
-	int		count;
-
-	i = 0;
-	count = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == '|' && i++ > 0)
-			count++;
-		if ((str[i] == '<' || str[i] == '>') && count++ > 0)
-		{
-			while (str[i] == '<' || str[i] == '>')
-				i++;
-		}
-		else if (str[i] != ' ')
-		{
-			count++;
-			while (str[i] != ' ' && str[i] != '\0'
-				&& str[i] != '|' && str[i] != '<' && str[i] != '>')
-				i++;
-		}
-		else
-			i++;
-	}
-	return (count);
-}
 
 static char	*ft_next_char(char *str)
 {
@@ -81,24 +22,29 @@ static char	*ft_next_char(char *str)
 	return (&str[i]);
 }
 
-static char	*ft_strndup(const char *s1, size_t n, t_memlist **stock)
+static int	quote_size(char *str)
 {
-	char	*result;
-	size_t	i;
+	int	i;
 
 	i = 0;
-	if (ft_strlen(s1) < n)
-		return (NULL);
-	result = stock_malloc(sizeof(char) * (n + 1), stock);
-	if (result == 0)
-		return (NULL);
-	while (i != n)
+	if (str[0] == '\'')
 	{
-		result[i] = s1[i];
 		i++;
+		while (str[i] && str[i] != '\'')
+			i++;
+		if (str[i] == '\'')
+			i++;
+		return (i);
 	}
-	result[i] = '\0';
-	return (result);
+	else
+	{
+		i++;
+		while (str[i] && str[i] != '\"')
+			i++;
+		if (str[i] == '\"')
+			i++;
+		return (i);
+	}
 }
 
 static int	word_len(char *str)
@@ -106,12 +52,14 @@ static int	word_len(char *str)
 	int	u;
 
 	u = 0;
-	while ((str[u] != ' ' && str[u]))
+	if (str[0] == '\'' || str[0] == '\"')
+		return (quote_size(str));
+	while ((str[u] != ' ' && str[u] && str[u] != '\'' && str[u] != '\"'))
 	{
 		if (u == 0 && (str[u] == '<' || str[u] == '>'))
 		{
 			while ((str[u] == '<' || str[u] == '>' || str[u] == '>')
-				&& str[u] != '|')
+				&& str[u] != '|' && str[u] != '\'' && str[u] != '\"')
 				u++;
 			break ;
 		}
@@ -124,29 +72,27 @@ static int	word_len(char *str)
 	return (u);
 }
 
-char	**shell_split(char *str, t_memlist **stock)
+t_list	*shell_split(char *str, t_memlist **stock)
 {
 	int		u;
-	int		i;
-	char	**result;
-	int		word_count;
+	char	*tmp;
+	t_list	*start;
 
 	if (!str)
 		return (NULL);
-	i = 0;
 	u = 0;
-	word_count = count_word(str);
-	result = stock_malloc(sizeof(char *) * (word_count + 1), stock);
-	if (!result)
-		return (NULL);
-	while (i != word_count)
+	start = ft_lstnew(NULL, stock);
+	while (str[0])
 	{
 		str = ft_next_char((char *)&str[u]);
 		u = word_len(str);
-		result[i] = ft_strndup(str, u, stock);
-		if (!result[i++])
+		tmp = ft_strndup(str, u, stock);
+		if (!tmp)
 			return (stock_free(stock), NULL);
+		printf("%s ", tmp);
+		ft_lstadd_back(&start, ft_lstnew(tmp, stock));
 	}
-	result[i] = NULL;
-	return (result);
+	printf("\n\n");
+	start = start->next;
+	return (start);
 }
